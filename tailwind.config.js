5f2764c1197a53f31ce34b835d88c35dce1eb518
IbrahimAliyev20 <iboisa12@gmail.com>
2025-04-21 10:55:05 +0400

    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,9 @@
 module.exports = {
-  mode: 'jit',
+  mode: 'jit', 
   content: [
-    './src/**/*.{html,js,jsx,ts,tsx}',
-    './index.html'
+    './**/*.{html,js,jsx,ts,tsx}',
+    './index.html',
   ],
-  safelist: [
-    { pattern: /^bg-[a-z]+-\d{1,3}$/ },
-    { pattern: /^text-[a-z]+-\d{1,3}$/ },
-    { pattern: /^flex(-.*)?$/ },
-    { pattern: /^grid(-.*)?$/ },
-    { pattern: /^gap-\d{1,2}$/ }, 
-    { pattern: /^p[trblxy]?-\d{1,2}$/ }, 
-    { pattern: /^m[trblxy]?-\d{1,2}$/ },
-    { pattern: /^w-(\d{1,2}|full)$/ }, 
-    { pattern: /^h-(\d{1,2}|full)$/ }, 
-    { pattern: /^rounded(-.*)?$/ }, 
-    { pattern: /^shadow(-.*)?$/ }, 
-  ],  
   theme: {
     extend: {},
   },
